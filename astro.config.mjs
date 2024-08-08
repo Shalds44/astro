11c230d6d2860a4623ab47d03781675db747f67b
Ronan lozahic <rlozahic@cogelec.fr>
2024-08-08 17:38:28 +0200

    preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import partytown from "@astrojs/partytown";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  integrations: [partytown(), preact()]
 });
